1a845c342f79c1d811c1a5d2b295d67388d554fe
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-06-12 11:00:59 -0300

    first commit

---
+++ 1_variaveis.js
@@ -0,0 +1,6 @@
+// Em JavaScript, você pode declarar variáveis usando as palavras-chave var, let ou const. var é a maneira mais antiga de declarar variáveis, mas ainda é usada. let e const são maneiras mais novas de declarar variáveis que foram introduzidas no ES6 (uma versão mais recente do JavaScript).
+
+let nome = "João";
+
+const idade = 25;
+
---
+++ 3_funcoes.js
@@ -0,0 +1,7 @@
+// As funções são blocos de código que podem ser reutilizados. Elas podem receber parâmetros e retornar um valor.
+
+function saudacao(nome) {
+    return "Olá, " + nome;
+}
+
+console.log(saudacao("João")); // Olá, João
