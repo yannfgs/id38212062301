a521b01caeaf575ec25254404c77040766706197
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-06-12 11:06:07 -0300

    12/06/2023 02

---
+++ 4_estruturasDeControle.js
@@ -0,0 +1,17 @@
+// JavaScript tem várias estruturas de controle, como if, for e while.
+
+let numero = 10;
+
+if (numero > 5) {
+    console.log("O número é maior que 5");
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log(i); // 0, 1, 2, 3, 4
+}
+
+let i = 0;
+while (i < 5) {
+    console.log(i); // 0, 1, 2, 3, 4
+    i++;
+}
