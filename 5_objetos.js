42d1e9a56b130d2975b0fb2bb7cc1b80ac5e2af1
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-06-12 18:30:50 -0300

    12/06/2023_03

---
+++ 5_objetos.js
@@ -0,0 +1,12 @@
+// Um objeto é uma coleção de propriedades, e uma propriedade é uma associação entre um nome (ou chave) e um valor.
+
+let pessoa = {
+    nome: "João",
+    idade: 25,
+    saudacao: function() {
+        return "Olá, " + this.nome;
+    }
+};
+
+console.log(pessoa.nome); // João
+console.log(pessoa.saudacao()); // Olá, João
