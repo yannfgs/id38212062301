560a6a61f0b5965eae70a39095ae506afb0a5c5a
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-06-13 09:30:58 -0300

    13/06/2023_01

--- 7_metodosDeArray.js
+++ 7_metodosDeArray.js
@@ -3,5 +3,16 @@
 let numeros [1, 2, 3, 4, 5];
 
 numeros.push(6); // adiciona um elemento ao final do array
+console.log(numeros); // [1, 2, 3, 4, 5, 6]
 
-)
+let ultimo = numeros.pop(); // remove e retorna o último elemento do array
+console.log(ultimo); // 6
+console.log(numeros); // [1, 2, 3, 4, 5]
+
+let primeiro = numeros.shift(); // remove e retorna o primeiro elemento do array
+console.log(primeiro); // 1
+console.log(numeros); // [2, 3, 4, 5]
+
+numeros.unshift(1); // adiciona um elemento ao início do array
+console.log(numeros); // [1, 2, 3, 4, 5]
+
