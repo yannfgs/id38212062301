cd23188dbf0751a1ee8b6be7345b83d12c78a926
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-06-13 11:49:24 -0300

    13/06/2023_05

---
+++ 11_operadores.js
@@ -0,0 +1,19 @@
+// JavaScript tem vários tipos de operadores, incluindo operadores aritméticos, de comparação e lógicos.
+
+let a = 10;
+let b = 20;
+
+console.log(a + b); //  30   (adição)
+console.log(a - b); //  -10  (subtração)
+console.log(a * b)  //  200  (multiplicação)
+console.log(a / b); //  0.5  (divisão)
+console.log(a % b); //  10   (módulo)
+
+console.log(a == b);    //  false   (igualdade)
+console.log(a != b);    //  true    (desigualdade)
+console.log(a < b);     //  true    (menor que)
+console.log(a > b);     //  false   (maior que)
+
+console.log(a && b);    //  20      (E lógico)
+console.log(a || b);    //  10      (OU lógico)
+console.log(!a);        //  false   (NÃO lógico)
---
+++ 12_templateStrings.js
@@ -0,0 +1,7 @@
+// Template strings são uma maneira de criar strings que podem incluir expressões JavaScript.
+
+let nome = "João";
+let idade = 25;
+
+let saudacao = `Olá, meu nome é ${nome} e eu tenho ${idade} anos.`;
+console.log(saudacao); // Olá, meu nome é João e eu tenho 25 anos.
