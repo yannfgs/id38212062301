8f9e8a589d37f19453a4240e494f966d09175e06
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-06-13 11:00:33 -0300

    13/06/2023_04

---
+++ 10_promisesAsyncAwait.js
@@ -0,0 +1,19 @@
+// Promises são uma maneira de lidar com operações assíncronas em JavaScript. Async/await é uma sintaxe que permite que você trabalhe com Promises de uma maneira mais confortável.
+
+let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Promessa cumprida!");
+    }, 1000);
+});
+
+promise.then(message => {
+    console.log(message); // "Promessa cumprida!"
+});
+
+// Aysnc/Await
+async function exemploAsyncAwait () {
+    let message = await promise;
+    console.log(message); // "Promessa cumprida!"
+}
+
+exemploAsyncAwait();
