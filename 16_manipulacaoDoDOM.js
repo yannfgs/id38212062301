4a78b76e842009e78d9a03ae8cc127402307a526
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-06-14 08:34:21 -0300

    14/06/2023_04

---
+++ 16_manipulacaoDoDOM.js
@@ -0,0 +1,11 @@
+// O Modelo de Objeto de Documento (DOM) é uma interface de programação para documentos HTML e XML. Ele representa a estrutura do documento e permite que os programas alterem o documento - adicionando, modificando ou excluindo elementos e conteúdo.
+
+// Suponha que temos o seguinte HTML:
+// <div id="meuDiv">Olá, mundo!</div>
+
+let div = document.getElementById("meuDiv"); // obtém o elemento com o id "meuDiv"
+console.log(div.textContent); // Olá, mundo!
+
+div.textContent = "Olá, JavaScript!"; // altera o conteúdo do div
+console.log(div.textContent); // Olá, JavaScript!
+
