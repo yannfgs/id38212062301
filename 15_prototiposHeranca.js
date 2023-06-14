1ff53f3bbeb54e991b93cbef920a4164c3d6655b
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-06-14 08:23:53 -0300

    14/06/2023_03

--- 15_prototiposHeranca.js
+++ 15_prototiposHeranca.js
@@ -1,2 +1,14 @@
 // JavaScript é uma linguagem baseada em protótipos, o que significa que a herança é realizada através de uma cadeia de protótipos.
 
+function Pessoa(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+}
+
+Pessoa.prototype.saudacao = function() {
+    return `Olá, meu nome é ${this.nome} e eu tenho ${this.idade} anos.`;
+}
+
+let joao = new Pessoa("João", 25);
+console.log(joao.saudacao()); // Olá, meu nome é João e eu tenho 25 anos.
+
