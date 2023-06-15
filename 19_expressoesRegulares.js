6d5f0a98ad5a76ff61478df66bf21c3922d17bdf
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-06-15 16:45:15 -0300

    15/06/2023_01

---
+++ 19_expressoesRegulares.js
@@ -0,0 +1,7 @@
+// As expressões regulares são padrões usados para combinar combinações de caracteres em strings. Em JavaScript, as expressões regulares também são objetos.
+
+let frase = "O gato gosta de caçar ratos.";
+let regex = /gato/g;
+
+let resultado = frase.match(regex);
+console.log(resultado); // ["gato"]
