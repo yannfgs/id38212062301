c6329268d32af38c9a2150f00318f196bd7da0af
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-06-21 08:57:13 -0300

    21/06/2023_04

--- 29_iteratorsAndGenerators.js
+++ 29_iteratorsAndGenerators.js
@@ -10,3 +10,17 @@ console.log(iterator.next()); // { value: 'b', done: false }
 console.log(iterator.next()); // { value: 'c', done: false }
 console.log(iterator.next()); // { value: undefined, done: true }
 
+
+// Generator
+function* generator() {
+    yield 'a';
+    yield 'b';
+    yield 'c';
+}
+
+let iterator2 = generator();
+
+console.log(iterator2.next()); // { value: 'a', done: false }
+console.log(iterator2.next()); // { value: 'b', done: false }
+console.log(iterator2.next()); // { value: 'c', done: false }
+console.log(iterator2.next()); // { value: undefined, done: true }
