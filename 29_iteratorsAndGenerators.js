85db95e91fa6063797a9ef0f882b03b77d644ad3
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-06-21 08:49:28 -0300

    21/06/2023_01

---
+++ 29_iteratorsAndGenerators.js
@@ -0,0 +1,3 @@
+// Iterators e Generators
+// Iterators são objetos que permitem iterar sobre os elementos de uma coleção (como um array ou uma string). Generators são funções especiais que podem pausar a execução e retomar posteriormente, permitindo criar iteradores de maneira mais fácil.
+
