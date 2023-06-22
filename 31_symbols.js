5505d3f7bb511ab8c60573a4e196974b8e77e457
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-06-22 17:53:34 -0300

    22062305

--- 31_symbols.js
+++ 31_symbols.js
@@ -1,3 +1,4 @@
 // Symbols
 // Symbols são um novo tipo de dados primitivo introduzido no ES6. Eles são únicos e imutáveis, e são usados principalmente como identificadores para propriedades de objetos.
 
+let sym1 = Symbol();
