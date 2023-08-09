0e405a0a752eb61bc29611bd2b617232ace33050
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-08-09 17:29:11 -0300

    09/08/23_03

--- 0_intro.js
+++ 0_intro.js
@@ -1,1 +1,25 @@
 // ARQUIVO DE ESTUDO JavaScript
+
+
+// Prompts que você pode usar para aprender JavaScript e alguns tópicos relacionados:
+
+// JavaScript
+
+// Escreva um programa JavaScript para encontrar o maior de três números.
+// Crie uma função JavaScript que aceita uma string como entrada e alterne as letras maiúsculas para minúsculas e vice-versa.
+// Implemente uma função em JavaScript para classificar uma matriz de números em ordem ascendente.
+// Banco de Dados
+
+// Escreva um script SQL para criar uma tabela de estudantes com colunas para ID, nome, e-mail e data de nascimento.
+// Como você usaria JavaScript para se conectar a um banco de dados SQL? Escreva um exemplo de código.
+// Crie um modelo de Mongoose para um blog que inclui campos para título, autor, conteúdo e data de publicação.
+// Express.js
+
+// Crie um servidor básico Express.js que responda "Olá Mundo!" para uma solicitação GET no caminho raiz.
+// Como você usaria o Express.js para servir arquivos estáticos, como imagens ou CSS?
+// Implemente uma API REST usando Express.js que suporte operações CRUD (Create, Read, Update, Delete) em uma coleção de itens.
+// EJS
+
+// Como você renderizaria uma variável JavaScript em uma visualização EJS?
+// Crie um exemplo simples de um aplicativo Express.js que usa EJS como mecanismo de visualização.
+// Como você usaria condicionais e loops em EJS? Escreva um exemplo de código.
