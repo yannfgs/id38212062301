5a437814fac61c92457c543f030738f937222cce
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-08-09 17:37:34 -0300

    09/08/23_05

---
+++ 33_programacaoFuncional.js
@@ -0,0 +1,11 @@
+// Programação Funcional
+
+// A programação funcional é um paradigma de programação que trata a computação como a avaliação de funções matemáticas e evita mudar o estado e dados mutáveis.
+
+const somar = (a, b) => a + b;
+const subtrair = (a, b) => a - b;
+
+const calcular = (operacao, a, b) => operacao(a, b);
+
+console.log(calcular(somar, 5, 3)); // 8
+console.log(calcular(subtrair, 5, 3)); // 2
