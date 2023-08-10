9dc051161ed33e12610f720db655c6c008228c11
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-08-10 11:35:03 -0300

    10/08/23_04

---
+++ 37_internacionalizacao(i18n).js
@@ -0,0 +1,12 @@
+// Internacionalização (i18n)
+
+// A internacionalização é o processo de desenvolver produtos de forma que possam ser facilmente adaptados para vários idiomas e regiões
+
+
+let valor = 123456.78;
+
+// Exibir o valor como moeda nos EUA
+console.log(new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(valor)); // $123,456.78
+
+// Exibir o valor como moeda no Brasil
+console.log(new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valor)); // R$ 123.456,78
