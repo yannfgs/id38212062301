841ffa16a30a8476d9bcde42879e2f8663035324
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-08-10 07:37:52 -0300

    10/08/23_02

---
+++ 35_webWorkers.js
@@ -0,0 +1,18 @@
+// Web Workers
+
+// Os Web Workers permitem executar código JavaScript em segundo plano, em uma thread separada da principal, evitando que a interface do usuário fique bloqueada.
+
+
+// arquivo: worker.js
+self.addEventListener('message', function(e) {
+    self.postMessage(e.data);
+} false);
+
+// arquivo principal
+let worker = new Worker('worker.js');
+
+worker.addEventListener('message', function(e) {
+    console.log('Worker disse: ' + e.data);
+}, false);
+
+worker.postMessage('Olá, Worker!');
