9a09e37e8b6e59f535517617564b5238f96f1142
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-08-11 08:20:53 -0300

    11/08/23_03

---
+++ 39_metodosDeNumero.js
@@ -0,0 +1,10 @@
+// Métodos de Número
+
+// Os números em JavaScript também têm métodos úteis.
+
+
+let numero = 123.456;
+
+console.log(numero.toFixed(2));         // "123.46"
+console.log(numero.toExponential(2));   // "1.23e+2"
+console.log(numero.toPrecision(4));     // "123.5"
