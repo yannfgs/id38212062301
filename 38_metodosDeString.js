506976e9ae08f2b5a4e0234d5b46ce3a5884e9a9
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-08-11 08:17:56 -0300

    11/08/23_02

--- 38_metodosDeString.js
+++ 38_metodosDeString.js
@@ -5,3 +5,9 @@
 
 let texto = "Olá mundo!";
 
+console.log(texto.toUpperCase());       // "OLÁ, MUNDO!"
+console.log(texto.toLowerCase());       // "olá, mundo!"
+console.log(texto.includes("mundo"));   // true
+console.log(texto.startsWith("Olá"));   // true
+console.log(texto.endsWith("!"));       // true
+console.log(texto.repeat(2));           // "Olá, mundo!Olá, mundo!"
