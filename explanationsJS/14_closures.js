3b14979b1e56049ce13986b5ac73cd8d0f00a1f0
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-08-19 11:02:42 -0300

    19/08/23_03

--- explanationsJS/14_closures.js
+++ explanationsJS/14_closures.js
@@ -1,7 +1,7 @@
 // Um closure é uma função que tem acesso ao escopo pai, mesmo depois que o escopo pai foi fechado.
 
 function fabricaDeSaudacao(saudacao) {
-    return function(nome) {
+    return function (nome) {
         return `${saudacao}, ${nome}!`;
     }
 }
