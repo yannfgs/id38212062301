b443b5f2c4cf6fdbc43351ad0c3da6d818aec8fd
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-08-22 17:10:07 -0300

    22/08/2023_04

--- explanationsJS/44_padraoObserver.js
+++ explanationsJS/44_padraoObserver.js
@@ -1,21 +1,20 @@
 class Observado {
     constructor() {
-      this.observadores = [];
+        this.observadores = [];
     }
-  
+
     inscrever(fn) {
-      this.observadores.push(fn);
+        this.observadores.push(fn);
     }
-  
+
     notificar(data) {
-      this.observadores.forEach(fn => fn(data));
+        this.observadores.forEach(fn => fn(data));
     }
-  }
-  
-  const obs = new Observado();
-  
-  obs.inscrever(data => console.log("Observador 1:", data));
-  obs.inscrever(data => console.log("Observador 2:", data));
-  
-  obs.notificar("Notificado!");
-  
+}
+
+const obs = new Observado();
+
+obs.inscrever(data => console.log("Observador 1:", data));
+obs.inscrever(data => console.log("Observador 2:", data));
+
+obs.notificar("Notificado!");
