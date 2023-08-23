e940e4a7239f1fb325c13fe4ce9a007c1e3dd9e7
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-08-23 10:08:38 -0300

    23/08/2023_04

---
+++ explanationsJS/48_coercionAndTruthyFalsy.js
@@ -0,0 +1,12 @@
+// Coercion e Truthy/Falsy
+
+// Coercion é a conversão automática ou implícita de valores de um tipo de dado para outro (como string para número). Valores truthy são valores que se traduzem em verdadeiro em um contexto booleano, e falsy são aqueles que se traduzem em falso.
+
+console.log(1 + "2"); // "12" (coerção de número para string)
+console.log(1 + "-2"); // -1 (coerção de string para número)
+
+if (" ") {
+    console.log("Espaços são truthy");
+}   else {
+    console.log("Espaços são falsy");
+}
