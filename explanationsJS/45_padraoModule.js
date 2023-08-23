c4fd1b5344e44852cfa0f67be634d9fd6ec046cd
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-08-23 08:07:26 -0300

    23/08/2023_01

--- explanationsJS/45_padraoModule.js
+++ explanationsJS/45_padraoModule.js
@@ -1,3 +1,7 @@
+// Padrão Module
+
+// O padrão Module é usado para criar módulos privados de código, o que pode evitar a contaminação do escopo global e fornecer encapsulamento.
+
 const MeuModulo = (() => {
     let privado = "Variável privada";
   
