2fa4c88e4a2e8afa3df976a7d18e2bac9905d570
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-08-28 07:37:26 -0300

    28/08/2023_01

---
+++ explanationsJS/49_JSON.js
@@ -0,0 +1,4 @@
+// JSON (JavaScript Object Notation)
+
+// JSON é um formato leve para armazenar e transportar dados. É frequentemente usado quando os dados estão sendo enviados de um servidor para um cliente da web.
+
