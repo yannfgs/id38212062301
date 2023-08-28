2db6d692f611c4149be48bbf94fd9e1d99fcaf16
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-08-28 07:37:42 -0300

    28/08/2023_02

--- explanationsJS/49_JSON.js
+++ explanationsJS/49_JSON.js
@@ -2,3 +2,14 @@
 
 // JSON é um formato leve para armazenar e transportar dados. É frequentemente usado quando os dados estão sendo enviados de um servidor para um cliente da web.
 
+let pessoa = {
+    nome: "João",
+    idade: 30
+  };
+  
+  let jsonString = JSON.stringify(pessoa); // Converte objeto para string JSON
+  console.log(jsonString); // '{"nome":"João","idade":30}'
+  
+  let jsonObject = JSON.parse(jsonString); // Converte string JSON para objeto
+  console.log(jsonObject.nome); // "João"
+  
