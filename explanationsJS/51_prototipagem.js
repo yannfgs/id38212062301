9e7e9a15922db9307fd03097b9395066858e6d3c
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-08-29 15:00:54 -0300

    29/08/2023_01

---
+++ explanationsJS/51_prototipagem.js
@@ -0,0 +1,16 @@
+// Prototipagem
+
+// Em JavaScript, cada objeto tem um "protótipo". Se você tentar acessar uma propriedade que não existe no objeto, JavaScript tentará encontrá-la no protótipo do objeto.
+
+
+function Pessoa(nome) {
+    this.nome = nome;
+  }
+  
+  Pessoa.prototype.cumprimentar = function() {
+    return "Olá, " + this.nome + "!";
+  };
+  
+  let joao = new Pessoa("João");
+  console.log(joao.cumprimentar()); // "Olá, João!"
+  
