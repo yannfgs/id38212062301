be1a7e032c19236e55c24274b2798c530d20bcba
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-09-03 19:31:32 -0300

    03/09/2023_03

---
+++ explanationsJS/54_closures.js
@@ -0,0 +1,12 @@
+function criarContador() {
+    let contador = 0;
+    return function() {
+      contador++;
+      return contador;
+    };
+  }
+  
+  const meuContador = criarContador();
+  console.log(meuContador()); // 1
+  console.log(meuContador()); // 2
+  
