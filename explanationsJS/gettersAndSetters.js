2b94305c6b06a42a959882b030c9e85c54423653
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-09-03 19:31:00 -0300

    03/09/2023_02

---
+++ explanationsJS/gettersAndSetters.js
@@ -0,0 +1,16 @@
+let pessoa = {
+    primeiroNome: "João",
+    ultimoNome: "Silva",
+    get nomeCompleto() {
+      return this.primeiroNome + " " + this.ultimoNome;
+    },
+    set nomeCompleto(valor) {
+      [this.primeiroNome, this.ultimoNome] = valor.split(" ");
+    }
+  };
+  
+  console.log(pessoa.nomeCompleto); // "João Silva"
+  pessoa.nomeCompleto = "Maria Oliveira";
+  console.log(pessoa.primeiroNome); // "Maria"
+  console.log(pessoa.ultimoNome); // "Oliveira"
+  
