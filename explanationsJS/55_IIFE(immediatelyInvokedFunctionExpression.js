9e6192ed1084e63cb632a75c79d97cff16b6e857
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-09-04 15:51:14 -0300

    04/09/2023_05

--- explanationsJS/55_IIFE(immediatelyInvokedFunctionExpression.js
+++ explanationsJS/55_IIFE(immediatelyInvokedFunctionExpression.js
@@ -2,10 +2,10 @@
 
 // Uma IIFE é uma função em JavaScript que é executada assim que é definida.
 
-(function() {
+(function () {
     let nome = "João";
     console.log(nome); // "João"
-  })();
-  
-  // A variável 'nome' não é acessível fora da IIFE
-  console.log(typeof nome); // "undefined"
+})();
+
+// A variável 'nome' não é acessível fora da IIFE
+console.log(typeof nome); // "undefined"
