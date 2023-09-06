472d491e7fc6954c4fd074157f464591a8fbf7ac
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-09-06 11:36:34 -0300

    06/09/2023_03

---
+++ explanationsJS/63_operadorTernario.js
@@ -0,0 +1,6 @@
+// Operador Ternário
+
+// O operador ternário é uma forma concisa de realizar uma verificação condicional. É frequentemente usado como um atalho para a instrução if. O operador ternário é composto por três partes: a condição, o resultado se a condição for verdadeira e o resultado se a condição for falsa. A condição é seguida por um ponto de interrogação (?), O resultado se verdadeiro é seguido por dois pontos (:), e o resultado se falso é o final da expressão. O operador ternário é frequentemente usado para atribuir um valor com base em uma condição. O operador ternário pode ser aninhado para realizar verificações condicionais mais complexas. 
+
+const valor = 10;
+const resultado = valor > 10 ? "maior que 10" : "menor ou igual a 10";
