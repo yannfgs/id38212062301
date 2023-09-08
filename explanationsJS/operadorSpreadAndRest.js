ec6d5add45b016a4d7c61e827425e9cd35d4a021
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-09-07 21:21:21 -0300

    07/09/2023_04

--- explanationsJS/operadorSpreadAndRest.js
+++ explanationsJS/operadorSpreadAndRest.js
@@ -5,6 +5,6 @@ console.log(arr2); // [1, 2, 3, 4, 5]
 
 // Rest
 function somar(...numeros) {
-  return numeros.reduce((acc, num) => acc + num, 0);
+    return numeros.reduce((acc, num) => acc + num, 0);
 }
 console.log(somar(1, 2, 3, 4)); // 10
