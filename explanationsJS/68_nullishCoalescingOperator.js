7d2f1e604f967fdc14524c28ccbb63ea86787e03
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-09-08 08:32:31 -0300

    08/09/2023_01

---
+++ explanationsJS/68_nullishCoalescingOperator.js
@@ -0,0 +1,7 @@
+// Nullish Coalescing Operator (??)
+
+// Este operador retorna o valor à direita quando o valor à esquerda é `null` ou `undefined`. Caso contrário, retorna o valor à esquerda.
+
+const nome = null;
+const saudacao = nome ?? "Visitante";
+console.log(saudacao); // Visitante
