1aa74f0215186069b9e32d80f2eb538abb5b9beb
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-09-07 21:22:06 -0300

    07/09/2023_05

---
+++ explanationsJS/67_asyncAwait.js
@@ -0,0 +1,11 @@
+async function buscarDados() {
+    try {
+      const response = await fetch('https://api.exemplo.com/dados');
+      const data = await response.json();
+      console.log(data);
+    } catch (erro) {
+      console.log('Erro:', erro);
+    }
+  }
+  buscarDados();
+  
