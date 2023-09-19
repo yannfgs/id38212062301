776bfaccc9f2e81bd62e785cdd154bd094081e08
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-09-19 11:20:33 -0300

    19/08/2023_03

---
+++ explanationsJS/72_strictMode.js
@@ -0,0 +1,6 @@
+// Strict Mode
+
+// O modo estrito faz com que você escreva um código mais "seguro", ao proibir algumas ações e destacar "erros silenciosos".
+
+"use strict";
+x = 3.14; // Isso causará um erro porque x não foi declarado
