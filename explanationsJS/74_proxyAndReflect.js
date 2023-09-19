704f19f9450494836cbd39ffd9f8bc51f1785fe7
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-09-19 11:29:35 -0300

    19/08/2023_05

---
+++ explanationsJS/74_proxyAndReflect.js
@@ -0,0 +1,13 @@
+// Proxy e Reflect
+
+// O objeto `Proxy` é usado para definir comportamentos personalizados para operações fundamentais (como pesquisa de propriedades). `Reflect` é um objeto embutido que fornece métodos para operações de objeto interceptáveis.
+
+const alvo = {};
+const manipulador = {
+    get: function(obj, prop) {
+        return prop in obj ? obj[prop] : "Valor padrão";
+    }
+}
+
+const proxy = new Proxy(alvo, manipulador);
+console.log(proxy.nome); // "Valor padrão"
