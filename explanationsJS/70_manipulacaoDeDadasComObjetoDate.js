86e99b3605c8d5823eef2f1c66583fc113cedbad
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-09-19 11:16:23 -0300

    19/08/2023_01

---
+++ explanationsJS/70_manipulacaoDeDadasComObjetoDate.js
@@ -0,0 +1,7 @@
+// Manipulação de Datas com o Objeto Date
+
+// O objeo `Date` em JavaScript é usado para manipular datas e horários.
+
+const agora = new Date();
+console.log(agora.getFullYear()); // Ano atual
+console.log(agora.getMonth()); // Mês atual (0-11)
