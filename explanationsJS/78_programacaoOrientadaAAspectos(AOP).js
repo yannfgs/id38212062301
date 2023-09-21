464ae8c4626a6ee483113989e4afa1d5490a66fd
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-09-21 15:56:50 -0300

    21/09/2023_03

---
+++ explanationsJS/78_programacaoOrientadaAAspectos(AOP).js
@@ -0,0 +1,15 @@
+// Programação Orientada a Aspectos (AOP)
+
+// AOP é um paradigma de programação que permite a separação de preocupações transversais em programas, como logging, segurança, etc.
+
+function executar(funcao, ...parametros) {
+    console.log(`Executando ${funcao.name}`);
+    return funcao(...parametros);
+}
+
+function somar(a, b) {
+    return a + b;
+}
+
+const resultado = executar(somar, 5, 3);
+console.log(`Resultado: ${resultado}`);
