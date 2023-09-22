e5cd9afe3a61fc34f1e72d7dd11d5266f58ac7be
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-09-22 11:42:52 -0300

    22/09/2023_03

---
+++ explanationsJS/81_errorHandlingComTryCatchFinally.js
@@ -0,0 +1,11 @@
+// Error Handling com Try/Catch/Finally
+
+// O bloco try permite testar um bloco de código para erros. O bloco catch permite manipular o erro, e o bloco finally permite executar código, após try e catch, independentemente do resultado. 
+
+try {
+    throw new Error('Um erro ocorreu');
+  } catch (erro) {
+    console.error(erro.message);
+  } finally {
+    console.log('Finalizando execução');
+  }  
