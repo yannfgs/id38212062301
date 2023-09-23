c53faafd950b5cb8955ae8b54c0c56957d16921a
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-09-23 10:20:39 -0300

    23/09/2023_03

--- explanationsJS/82_object.freezeAndObjec.seal.js
+++ explanationsJS/82_object.freezeAndObjec.seal.js
@@ -2,3 +2,14 @@
 
 // Object.freeze e Object.seal são métodos usados para restringir a modificação de objetos.
 
+const obj = { prop: 42 };
+Object.freeze(obj);
+obj.prop = 33;  // Não altera o valor
+console.log(obj.prop);  // 42
+
+const obj2 = { prop: 42 };
+Object.seal(obj2);
+obj2.prop = 33;  // Altera o valor
+console.log(obj2.prop);  // 33
+obj2.novaProp = "teste";  // Não adiciona nova propriedade
+console.log(obj2.novaProp);  // undefined
