29c1b576e4bb330863540fef6fdc12b58cc06aa3
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-09-23 10:21:10 -0300

    23/09/2023_04

---
+++ explanationsJS/83_metodosEstaticos.js
@@ -0,0 +1,12 @@
+// Métodos Estáticos
+
+// Métodos estáticos são definidos na própria classe, não em instâncias da classe, e são chamados sem a instanciação da classe.
+
+class MinhaClasse {
+    static metodoEstatico() {
+      return 'Método estático foi chamado';
+    }
+  }
+  
+  console.log(MinhaClasse.metodoEstatico());
+  
