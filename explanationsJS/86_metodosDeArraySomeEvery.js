cc571d67f3ff4d820adafc559c07548a98d90d8c
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-09-25 13:11:22 -0300

    25/09/2023-03

---
+++ explanationsJS/86_metodosDeArraySomeEvery.js
@@ -0,0 +1,15 @@
+// Métodos de Array: some e every
+
+// Os métodos `some` e `every` são utilizados para testar elementos em um array. O método `some` retorna `true` se pelo menos um elemento satisfizer a condição, enquanto `every` retorna `true` se todos os elementos satisfizerem a condição.
+
+const array = [1, 2, 3, 4, 5];
+
+const algumMaiorQue3 = array.some(num => num > 3);
+console.log(algumMaiorQue3);  // true
+
+const todosMaiorQue0 = array.every(num => num > 0);
+console.log(todosMaiorQue0);  // true
+
+const algumMaiorQue6 = array.some(num => num > 6);
+console.log(algumMaiorQue6); // false
+
