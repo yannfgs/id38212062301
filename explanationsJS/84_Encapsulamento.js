064719e9dceb0c1df797731c8179f13cdf39b866
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-09-25 13:06:51 -0300

    25/09/2023-01

---
+++ explanationsJS/84_Encapsulamento.js
@@ -0,0 +1,23 @@
+// Encapsulamento
+
+// Encapsulamento é um princípio que restringe o acesso direto aos membros de um objeto, o que é uma forma de prevenir acessos acidentais ou indevidos ao código interno.
+
+class Pessoa {
+    constructor(nome) {
+      this._nome = nome;
+    }
+  
+    get nome() {
+      return this._nome;
+    }
+  
+    set nome(novoNome) {
+      this._nome = novoNome;
+    }
+  }
+  
+  const p = new Pessoa('João');
+  console.log(p.nome);  // João
+  p.nome = 'Maria';
+  console.log(p.nome);  // Maria
+  
