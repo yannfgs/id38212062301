0cf4819b82f5601376f13e7497b9e5dc3eef0c58
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-09 17:21:16 -0300

    09102304

---
+++ explanationsJS/89_operadorDeEncadeamentoOpcional.js
@@ -0,0 +1,6 @@
+// O operador de encadeamento opcional permite a leitura segura de propriedades aninhadas de um objeto.
+
+const pessoa = { nome: { primeiro: 'João' } };
+
+const sobrenome = pessoa.nome?.sobrenome;
+console.log(sobrenome); // undefined
