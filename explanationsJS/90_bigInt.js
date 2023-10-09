03bd76c96ca215038f030e7dea44a4dd3d99f13d
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-09 17:22:56 -0300

    09102305

---
+++ explanationsJS/90_bigInt.js
@@ -0,0 +1,4 @@
+// `BigInt` é um tipo de dado numérico que pode representar inteiros de tamanho arbitrário.
+
+const grandeNumero = BigInt("123456789012345678901234567890");
+console.log(grandeNumero); // 123456789012345678901234567890n
