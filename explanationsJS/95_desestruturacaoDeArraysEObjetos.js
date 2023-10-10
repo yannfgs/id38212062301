923f5a9ac6eaf32000120262a312d05c7af64a89
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-10 08:07:10 -0300

    10102304

---
+++ explanationsJS/95_desestruturacaoDeArraysEObjetos.js
@@ -0,0 +1,9 @@
+// Desestruturação de Arrays e Objetos
+
+// A desestruturação é uma maneira conveniente de extrair valores de arrays ou propriedades de objetos.
+
+const [a, b] = [1, 2];
+console.log(a, b); // 1 2
+
+const { x, y } = { x: 10, y: 20 };
+console.log(x, y); // 10 20
