02c18b3bd547eeaaff1ab89f2938138bc3c3b307
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-14 21:06:57 -0300

    14102301

--- explanationsJS/102_operadorInstanceOf.js
+++ explanationsJS/102_operadorInstanceOf.js
@@ -2,7 +2,7 @@
 
 // instanceof verifica se o protótipo de um objeto aparece em qualquer lugar na cadeia de protótipos do outro objeto.
 
-class Pessoa {}
+class Pessoa { }
 const joao = new Pessoa();
 
 // Verifica se joao é uma instância de Pessoa
