e9db07c7486bc0c9bc176ee3cca2ac8fcc163e88
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-14 21:08:18 -0300

    14102303

--- explanationsJS/103_funcoesDeCallback.js
+++ explanationsJS/103_funcoesDeCallback.js
@@ -1,2 +1,14 @@
+// Funções de Callback
+
 // Uma função de callback é uma função passada como argumento para outra função, sendo chamada quando a operação termina, para evitar bloqueio.
 
+function saudacao(nome, callback) {
+    console.log(`Olá ${nome}`);
+    callback();
+  }
+  
+  // A função de callback é chamada após a saudação
+  saudacao("João", function() {
+    console.log("Callback foi chamada");
+  });
+  
