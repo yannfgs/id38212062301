4c8925e5730503cd8c8fe81d8722e1b28cf991a4
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-17 16:48:55 -0300

    17102304

---
+++ explanationsJS/108_EventLoopAndFilaDeTarefas.js
@@ -0,0 +1,10 @@
+// Event Loop e Fila de Tarefas
+
+// O JavaScript utiliza um modelo de concorrência baseado em um "event loop" para executar código, coletar eventos e executar tarefas de maneira assíncrona.
+
+console.log('Início');
+setTimeout(() => {
+    console.log('Timeout');
+}, 0);
+console.log('Fim');
+// Saída: Início, Fim, Timeout
