cce18bab1e3c0233a42007ba981f3944ab95603b
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-17 16:46:11 -0300

    17102302

---
+++ explanationsJS/107_manipulacaoDeDOM.js
@@ -0,0 +1,8 @@
+// Manipulação de DOM
+
+// Manipulação de DOM refere-se à prática de usar JavaScript para criar, modificar e excluir elementos HTML e CSS.
+
+// Cria um novo elemento p e adiciona ao body
+const paragrafo = document.createElement('p');
+paragrafo.textContent = 'Novo parágrafo';
+document.body.appendChild(paragrafo;)
