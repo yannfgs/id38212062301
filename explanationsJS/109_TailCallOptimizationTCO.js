f73a9f807098685c5b167d95e396b24c69661476
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-18 18:10:37 -0300

    18102302

--- explanationsJS/109_TailCallOptimizationTCO.js
+++ explanationsJS/109_TailCallOptimizationTCO.js
@@ -1,1 +1,4 @@
 // Tail Call Optimization (TCO)
+
+// A otimização de chamada de cauda é um recurso em que uma função chamada como a última ação de outra função não cria um novo quadro de pilha, economizando memória.
+
