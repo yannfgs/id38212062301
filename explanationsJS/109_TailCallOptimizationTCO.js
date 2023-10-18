c6d1d936bd6e83d6007c5e6c6d870107fe61d21f
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-18 18:10:47 -0300

    18102303

--- explanationsJS/109_TailCallOptimizationTCO.js
+++ explanationsJS/109_TailCallOptimizationTCO.js
@@ -2,3 +2,12 @@
 
 // A otimização de chamada de cauda é um recurso em que uma função chamada como a última ação de outra função não cria um novo quadro de pilha, economizando memória.
 
+function fatorial(n, acumulador = 1) {
+    if (n === 0) {
+        return acumulador;
+    }
+    return fatorial(n - 1, n * acumulador);
+}
+
+// Esta função é otimizada com TCO, pois a última ação é uma chamada de função
+console.log(fatorial(5));  // 120
