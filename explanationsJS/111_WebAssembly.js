e8e0b95bc941ebd842468e4210923d571d0d30ad
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-19 17:40:13 -0300

    19102305

--- explanationsJS/111_WebAssembly.js
+++ explanationsJS/111_WebAssembly.js
@@ -2,3 +2,14 @@
 
 // WebAssembly é um formato de código binário para programas executáveis que permite executar código de alto desempenho no navegador.
 
+// Exemplo simples de carregar um módulo WebAssembly.
+async function carregarWebAssembly() {
+    const resposta = await fetch('meu_modulo.wasm');
+    const buffer = await resposta.arrayBuffer();
+    const modulo = await WebAssembly.compile(buffer);
+    return new WebAssembly.Instance(modulo);
+}
+
+carregarWebAssembly().then(instance => {
+    console.log(instance.exports.minhaFuncao());
+});
