70f4e0cfbf2f4b99b5b9fa69d13c8666b48d139e
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-19 17:39:27 -0300

    19102303

--- explanationsJS/110_JavaScriptEngines.js
+++ explanationsJS/110_JavaScriptEngines.js
@@ -2,3 +2,6 @@
 
 // Motores JavaScript, como V8 (Google Chrome) e SpiderMonkey (Firefox), são responsáveis por converter código JavaScript em código de máquina eficiente.
 
+
+
+// Não há um exemplo de código específico para este tópico, pois trata-se de como o JS é processado internamente pelos navegadores.
