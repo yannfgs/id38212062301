f308d849db6e31979496e1e8ac1036c5a9aa664f
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-20 18:07:36 -0300

    20102302

--- explanationsJS/112_ServiceWorkers.js
+++ explanationsJS/112_ServiceWorkers.js
@@ -2,3 +2,13 @@
 
 // Service workers atuam como proxies de rede e são úteis para recursos offline, interceptando requisições e gerenciando respostas do cache.
 
+// Registrando um service worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => {
+            console.log('Service Worker registrado com sucesso:', registration);
+        })
+        .catch(erro => {
+            console.log('Falha ao registrar o Service Worker:', erro);
+        });
+}
