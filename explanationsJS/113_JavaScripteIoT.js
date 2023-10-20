a7b0b3303de2e2a92036f6ae152e37eba0586e44
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-20 18:08:32 -0300

    20102304

--- explanationsJS/113_JavaScripteIoT.js
+++ explanationsJS/113_JavaScripteIoT.js
@@ -2,3 +2,17 @@
 
 // JavaScript não é usado apenas para web, mas também em dispositivos IoT, como com a plataforma Johnny-Five para robótica e IoT.
 
+// Código usando Johnny-Five para ler dados de um sensor de temperatura
+const { Board, Thermometer } = require("johnny-five");
+const board = new Board();
+
+board.on("ready", () => {
+    const termometro = new Thermometer({
+        controller: "LM35",
+        pin: "A0"
+    });
+
+    termometro.on("change", () => {
+        console.log(`${termometro.celsius}°C`);
+    });
+});
