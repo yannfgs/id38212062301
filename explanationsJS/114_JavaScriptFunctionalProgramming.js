6c770ec054c1cf4c8001f7812643ad1013ea95c9
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-21 09:28:02 -0300

    21102301

---
+++ explanationsJS/114_JavaScriptFunctionalProgramming.js
@@ -0,0 +1,16 @@
+// JavaScript Functional Programming
+
+// Programação funcional em JavaScript envolve o uso de funções puras, funções de alta ordeem e imutabilidade.
+
+
+// Função pura
+function somar(a, b) {
+    return a + b;
+}
+
+// Função de alta ordem
+function executarEPrintar(funcao, ...args) {
+    console.log(funcao(...args));
+}
+
+executarEPrintar(somar, 5, 10); // 15
