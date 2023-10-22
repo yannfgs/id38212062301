2c6e6789d0acdf4b03c4d21d5da2799b847d2d3f
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-22 10:52:42 -0300

    22102304

--- explanationsJS/117_JavaScripteMachineLearning.js
+++ explanationsJS/117_JavaScripteMachineLearning.js
@@ -2,3 +2,12 @@
 
 // Com bibliotecas como TensorFlow.js, é possível executar modelos de machine learning no navegador ou em Node.js.
 
+
+// Exemplo usando TensorFlow.js
+async function executarModelo() {
+    const modelo = await tf.loadLayersModel('https://.../modelo/model.json');
+    const predicao = modelo.predict(tf.tensor2d([5], [1, 1]));
+    predicao.print();
+}
+
+executarModelo();
