0a7ff8566529cec057f27343a2a4f06553da87b2
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-23 17:11:25 -0300

    23102302

--- explanationsJS/118_JavaScripteComputaçãoGráfica.js
+++ explanationsJS/118_JavaScripteComputaçãoGráfica.js
@@ -11,5 +11,18 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const geometria = new THREE.BoxGeometry();
+const materal = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cubo = new THREE.Mesh(geometria, material);
 
+cena.add(cubo);
+camera.position.z = 5;
 
+function animar() {
+    requestAnimationFrame(animar);
+    cubo.rotation.x += 0.01;
+    cubo.rotation.y += 0.01;
+    renderer.render(cena, camera);
+}
+
+animar();
