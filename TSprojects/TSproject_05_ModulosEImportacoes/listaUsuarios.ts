4e30614e7a01e2a41c9dd77d9fd6eae25515ce90
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-26 14:09:34 -0300

    26102301

---
+++ TSprojects/TSproject_05_ModulosEImportacoes/listaUsuarios.ts
@@ -0,0 +1,6 @@
+// Agora, definiremos um módulo para gerenciar uma lista de usuários.
+
+// listaUsuarios.ts
+importa { Usuario } from './user'; // Importa a classe 'Usuario' do arquivo 'user.ts'.
+
+export class listaUsuarios { // Exporta a classe 'ListaUsuarios' para uso }
--- TSprojects/TSproject_05_ModulosEImportacoes/user.ts
+++ TSprojects/TSproject_05_ModulosEImportacoes/user.ts
@@ -4,3 +4,4 @@
 export class Usuario { // 'export' torna a classe 'Usuario' disponível para importação em outros arquivos.
     constructor(public nome: string, public email: string) {} // Inidializa as propriedades 'nome' e 'email' através do construtor.
 }
+
