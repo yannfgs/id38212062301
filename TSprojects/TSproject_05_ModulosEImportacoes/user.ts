3f8218b0065be279f3fb97a6c30cea0f7522f688
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-26 14:10:32 -0300

    26102302

--- TSprojects/TSproject_05_ModulosEImportacoes/user.ts
+++ TSprojects/TSproject_05_ModulosEImportacoes/user.ts
@@ -2,6 +2,6 @@
 
 // user.ts
 export class Usuario { // 'export' torna a classe 'Usuario' disponível para importação em outros arquivos.
-    constructor(public nome: string, public email: string) {} // Inidializa as propriedades 'nome' e 'email' através do construtor.
+    constructor(public nome: string, public email: string) { } // Inidializa as propriedades 'nome' e 'email' através do construtor.
 }
 
