c0884955eacda9912a5e63543d4a1ee798b92909
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-30 15:53:46 -0300

    30102301

---
+++ explanationsJS/119_JavaScriptERealidadeVirtual.js
@@ -0,0 +1,14 @@
+// Este é um exemplo de marcação HTML usando A-Frame para criar uma cena de realidade virtual básica.
+// Deve ser colocado dentro de um arquivo HTML.
+
+// <html>
+//   <head>
+//     <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
+//   </head>
+//   <body>
+//     <a-scene>
+//       <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+//       <a-sky color="#ECECEC"></a-sky>
+//     </a-scene>
+//   </body>
+// </html>
