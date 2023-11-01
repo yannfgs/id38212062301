19e819240b01f8dd3a03b8de34633748283c2c1a
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-11-01 17:29:28 -0300

    01112303

---
+++ explanationsJS/124_TrabalhandoComLocalStorageEmJavaScript.js
@@ -0,0 +1,6 @@
+// Trabalhando com Local Storage em JavaScript
+
+// Local Storage permite que dados sejam armazenados no navegador e persistam mesmo após o navegador ser fechado.
+
+
+// Salvando e recuperando dados com Local Storage
