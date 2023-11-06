ace07a70f23ee172b329722007ac853a818042b1
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-11-06 13:06:30 -0300

    06112302

---
+++ explanationsJS/128_JavaScriptEAcessibilidade(a11y).js
@@ -0,0 +1,9 @@
+// JavaScript e Acessibilidade (a11y)
+
+// Acessibilidade na web significa permitir que todos os usuários acessem conteúdos, incluindo pessoas com deficiência.
+
+
+// Este script adiciona um atributo 'alt' a todas as imagens sem um
+document.querySelectorAll('img:not([alt])').forEach(img => {
+    img.setAttribute('alt', 'Descrição da imagem');
+});
