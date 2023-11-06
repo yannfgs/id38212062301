580d5930f21590acaa1b5ef04bc3197754c2f3c2
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-11-06 13:08:44 -0300

    06112304

---
+++ explanationsJS/130_JavaScriptESegurancaWeb.js
@@ -0,0 +1,6 @@
+// JavaScript e Segurança Web
+
+// Segurança na web é crucial, envolvendo práticas como evitar vazamento de dados, proteger contra XSS e CSRF, e garantir comunicação segura.
+
+
+// Não há um exemplo de código específico para este tópico, pois trata-se de práticas de segurança e configurações de servidor.
