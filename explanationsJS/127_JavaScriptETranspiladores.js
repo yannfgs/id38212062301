2e688f3fee82b30abd8b1d8073d27b1ef3f9583f
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-11-06 13:04:25 -0300

    06112301

---
+++ explanationsJS/127_JavaScriptETranspiladores.js
@@ -0,0 +1,19 @@
+// JavaScript e Transpiladores
+
+// Transpiladores como Babel permitem que você use recursos do JavaScript moderno que ainda não são suportados em todos os navegadores.
+
+
+// O código abaixo usa a sintaxe de classe do ES6, que não é suportada em navegadores mais antigos.
+// Babel pode converter isso para uma função construtora que é amplamente suportada.
+
+class MinhaClasse {
+    constructor(nome) {
+      this.nome = nome;
+    }
+  }
+  
+  // O código transpilado pelo Babel se parecerá com:
+  // function MinhaClasse(nome) {
+  //   this.nome = nome;
+  // }
+  
