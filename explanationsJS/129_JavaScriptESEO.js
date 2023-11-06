137e87f789e829409a45bea0adefcea4ac8f74e9
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-11-06 13:07:45 -0300

    06112303

---
+++ explanationsJS/129_JavaScriptESEO.js
@@ -0,0 +1,6 @@
+// JavaScript e SEO
+
+// JavaScript moderno com renderização do lado do cliente pode apresentar desafios para SEO, mas soluções como renderização do lado do servidor (SSR) ou prerenderização podem ajudar.
+
+
+// Não há um exemplo de código específico para este tópico, pois trata-se de práticas de desenvolvimento e configuração do servidor.
