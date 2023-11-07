9a81c9322c42cb70d900a6be10c11dfcdf04d651
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-11-07 17:26:13 -0300

    07112301

---
+++ exercisesAndPracticeJS/indexGeneral.js
@@ -0,0 +1,6 @@
+/* 
+
+JavaScript é uma linguagem de programação que permite adicionar interatividade aos sites. TypeScript é um superset de JavaScript, o que significa que ele adiciona funcionalidades extras à linguagem, como tipos estáticos, que podem ajudar a prevenir erros durante o desenvolvimento. React é uma biblioteca JavaScript para construir interfaces de usuário, especialmente útil para criar single-page applications.
+
+*/
+
