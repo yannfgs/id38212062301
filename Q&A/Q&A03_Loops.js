4e813d03f2e7f3f89d42d4ba605a3e95a8b65cfe
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-11-17 07:55:38 -0300

    17112301

---
+++ Q&A/Q&A03_Loops.js
@@ -0,0 +1,16 @@
+/* 
+
+-- Pergunta 3: O que são e como funcionam os loops em JavaScript?
+
+Loops são usados para executar repetidamente um bloco de código enquanto uma condição especificada é verdadeira. Os tipos mais comuns de loops em JavaScript são `for`, `while` e `do...while`.
+
+*/
+
+// Exemplo com for:
+for (let i = 0; i < 5; i++) {
+    console.log(i); // Este código exibe os números de 0 a 4 no console
+}
+
+// `let i = 0;` é a inicialização. Aqui declaramos a variável `i` e inicializamos com 0.
+// `i < 5;` é a condição. O loop continuará enquanto `i` for menor que 5.
+// `i++` é o incremento. Após cada iteração do loop, incrementamos `i` em 1.
---
+++ Q&A/Q&A04_FuncoesJavaScript.js
@@ -0,0 +1,8 @@
+/*
+
+Pergunta 4: O que são funções em JavaScript e como criar uma?
+
+Uma função em JavaScript é um bloco de código projetado para executar uma tarefa específica. Funções são úteis para reutilizar código. Para criar uma função, você usa a palavra-chave `function`, seguida por um nome, parênteses `()`, e um bloco de código `{}`.
+
+*/
+
