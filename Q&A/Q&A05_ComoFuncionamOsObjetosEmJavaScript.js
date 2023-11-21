b7225a165191342eb05ba71faa9f875700df48b9
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-11-21 16:55:53 -0300

    21112303

---
+++ Q&A/Q&A05_ComoFuncionamOsObjetosEmJavaScript.js
@@ -0,0 +1,19 @@
+/*
+
+Pergunta 5: Como funcionam os objetos em JavaScript?
+
+Em JavaScript, um objeto é uma coleção de propriedades, e uma propriedade é uma associação entre um nome (ou chave) e um valor. Um valor de propriedade pode ser uma função, que é então considerada um método do objeto.
+
+*/
+
+// Exemplo de um Objeto:
+let pessoa = {
+    nome: "Yann",
+    idade: 30,
+    falar: function() {
+        console.log("Olá, meu nome é " + this.nome); // Usa 'this.nome' para acessar a propriedade 'nome' do objeto
+    }
+};
+
+console.log(pessoa.nome); // Acessa a propriedade 'nome' do objeto 'pessoa'
+pessoa.falar(); // Chama o método 'falar' do objeto 'pessoa'
