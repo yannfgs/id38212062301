c852b76783593d9f180cb3712708ebedf8ba2968
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-12-18 18:01:20 -0300

    18122304

--- explanationsJS/38_metodosDeString.js
+++ explanationsJS/38_metodosDeString.js
@@ -2,12 +2,11 @@
 
 // Javascript oferece uma variedade de métodos úteis para trabalhar com strings.
 
-
 let texto = "Olá mundo!";
 
-console.log(texto.toUpperCase());       // "OLÁ, MUNDO!"
-console.log(texto.toLowerCase());       // "olá, mundo!"
-console.log(texto.includes("mundo"));   // true
-console.log(texto.startsWith("Olá"));   // true
-console.log(texto.endsWith("!"));       // true
-console.log(texto.repeat(2));           // "Olá, mundo!Olá, mundo!"
+console.log(texto.toUpperCase()); // "OLÁ, MUNDO!"
+console.log(texto.toLowerCase()); // "olá, mundo!"
+console.log(texto.includes("mundo")); // true
+console.log(texto.startsWith("Olá")); // true
+console.log(texto.endsWith("!")); // true
+console.log(texto.repeat(2)); // "Olá, mundo!Olá, mundo!"
