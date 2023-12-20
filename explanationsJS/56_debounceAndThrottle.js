93d8ed2aa5ebaf6f9aa1d702ceabf709c430a3fc
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-12-20 11:37:42 -0300

    20122301

--- explanationsJS/56_debounceAndThrottle.js
+++ explanationsJS/56_debounceAndThrottle.js
@@ -2,32 +2,31 @@
 
 // Debounce e Throttle são técnicas para controlar a frequência com que uma função é executada.
 
-
 // Função debounce
 function debounce(func, delay) {
-    let debounceTimer;
-    return function (...args) {
-        clearTimeout(debounceTimer);
-        debounceTimer = setTimeout(() => func.apply(this, args), delay);
-    };
+  let debounceTimer;
+  return function (...args) {
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(() => func.apply(this, args), delay);
+  };
 }
 
 const minhaFuncao = debounce(() => {
-    console.log("Função executada após 300ms");
+  console.log("Função executada após 300ms");
 }, 300);
 
 // Função throttle
 function throttle(func, limit) {
-    let inThrottle;
-    return function (...args) {
-        if (!inThrottle) {
-            func.apply(this, args);
-            inThrottle = true;
-            setTimeout(() => (inThrottle = false), limit);
-        }
-    };
+  let inThrottle;
+  return function (...args) {
+    if (!inThrottle) {
+      func.apply(this, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
 }
 
 const minhaFuncaoThrottle = throttle(() => {
-    console.log("Função executada no máximo uma vez a cada 300ms");
+  console.log("Função executada no máximo uma vez a cada 300ms");
 }, 300);
