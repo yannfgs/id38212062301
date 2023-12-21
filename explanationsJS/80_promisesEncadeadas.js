14d44972979daa87aa17bc57083e4505ca06a5e1
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-12-21 17:28:11 -0300

    21122302

--- explanationsJS/80_promisesEncadeadas.js
+++ explanationsJS/80_promisesEncadeadas.js
@@ -2,12 +2,12 @@
 
 // Promises podem ser encadeadas para executar tarefas assíncronas em sequência.
 
-fetch('https://api.exemplo.com/dados')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        return fetch('https://api.exemplo.com/mais-dados');
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(erro => console.error('Erro', erro));
+fetch("https://api.exemplo.com/dados")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    return fetch("https://api.exemplo.com/mais-dados");
+  })
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((erro) => console.error("Erro", erro));
