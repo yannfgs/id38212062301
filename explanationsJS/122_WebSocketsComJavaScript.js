563f65519919d27016c0634ba727e88d7c0678d2
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-12-21 17:27:39 -0300

    21122301

--- explanationsJS/122_WebSocketsComJavaScript.js
+++ explanationsJS/122_WebSocketsComJavaScript.js
@@ -2,14 +2,13 @@
 
 // WebSockets proporcionam uma comunicação bidirecional entre o cliente e o servidor, permitindo interações em tempo real.
 
-
 // Criando um WebSocket para se comunicar com um servidor
-const socket = new WebSocket('ws://localhost:8080');
+const socket = new WebSocket("ws://localhost:8080");
 
 socket.onopen = function (event) {
-    socket.send('Olá, servidor!');
+  socket.send("Olá, servidor!");
 };
 
 socket.onmessage = function (event) {
-    console.log('Mensagem recebida do servidor:', event.data);
+  console.log("Mensagem recebida do servidor:", event.data);
 };
