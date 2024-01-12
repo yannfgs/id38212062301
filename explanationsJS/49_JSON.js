13ad27162619b32822f98a746ffb385d4188918e
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-01-12 18:08:46 -0300

    12012402

--- explanationsJS/49_JSON.js
+++ explanationsJS/49_JSON.js
@@ -2,6 +2,7 @@
 
 // JSON é um formato leve para armazenar e transportar dados. É frequentemente usado quando os dados estão sendo enviados de um servidor para um cliente da web.
 
+
 let pessoa = {
     nome: "João",
     idade: 30
