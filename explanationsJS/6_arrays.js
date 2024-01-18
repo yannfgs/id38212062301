b1e2245460d1055217201366b1b5aaacc54b93e1
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-01-18 17:48:49 -0300

    18012404

--- explanationsJS/6_arrays.js
+++ explanationsJS/6_arrays.js
@@ -1,6 +1,7 @@
 // Um array é uma estrutura de dados que armazena uma coleção de elementos. Esses elementos podem ser de qualquer tipo, incluindo números, strings e outros objetos.
 // A contagem de arrays se inicia no 0, portanto a posição 1 do array é o segundo elemento.
 
+
 let numeros = [1, 2, 3, 4, 5];
 console.log(numeros[0]); // 1
 
