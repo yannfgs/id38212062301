4857649abbb4dd3e33e6ca40c2c92f3fa94f55d4
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-02-02 17:28:04 -0300

    02022401

--- explanationsJS/8_mapFilterReduce.js
+++ explanationsJS/8_mapFilterReduce.js
@@ -1,5 +1,6 @@
 // Estes são métodos de array muito úteis que permitem que você processe os elementos de um array de maneira funcional e imutável.
 
+
 let numeros = [1, 2, 3, 4, 5];
 
 let quadrados = numeros.map(n => n * n); // [1, 4, 9, 16, 25]
