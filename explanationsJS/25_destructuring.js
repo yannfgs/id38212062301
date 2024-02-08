16e5bf8b17c7295839463a98992db41a0cacf63b
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-02-08 17:44:37 -0300

    08022403

--- explanationsJS/25_destructuring.js
+++ explanationsJS/25_destructuring.js
@@ -2,7 +2,6 @@
 
 // O Destructuring é uma maneira conveniente de extrair valores de arrays ou propriedades de objetos em suas próprias variáveis.
 
-
 let [a, b, c] = [1, 2, 3];
 console.log(a); // 1
 console.log(b); // 2
