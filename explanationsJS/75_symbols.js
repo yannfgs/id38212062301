ed8c40116b2090745d9d1b478747dba15d0f8d48
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-02-15 17:25:50 -0300

    15022401

--- explanationsJS/75_symbols.js
+++ explanationsJS/75_symbols.js
@@ -2,6 +2,7 @@
 
 // Symbol é um tipo de dado cujas instâncias são únicas e imutáveis. Symbols são frequentemente usados como identificadores de propriedades de objeto para evitar conflitos.
 
+
 const simbolo = Symbol("descricao");
 const objeto = {
   [simbolo]: "Valor do simbolo"
