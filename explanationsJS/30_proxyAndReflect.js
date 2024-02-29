bb8b2447f451405f6e204c9b54a425f8d9159ee2
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-02-29 17:49:57 -0300

    29022401

--- explanationsJS/30_proxyAndReflect.js
+++ explanationsJS/30_proxyAndReflect.js
@@ -2,7 +2,6 @@
 
 // O objeto Proxy é usado para definir comportamentos personalizados para operações fundamentais (por exemplo, pesquisa de propriedades, atribuição, enumeração, invocação de função, etc.). O objeto Reflect é usado para manipular objetos e tem métodos correspondentes a quase todos os manipuladores de proxy.
 
-
 let target = {};
 let handler = {
     get: function (target, prop, receiver) {
