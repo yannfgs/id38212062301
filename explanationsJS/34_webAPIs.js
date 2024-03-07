2264fd5c72fedb0ec4f8af5e40d214b73a14f4d7
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-03-07 17:50:08 -0300

    07032401

--- explanationsJS/34_webAPIs.js
+++ explanationsJS/34_webAPIs.js
@@ -2,7 +2,6 @@
 
 // As APIs fornecem funcionalidades para interagir com o navegador e o ambiente de hospedagem. Exemplos incluem manipulação do DOM, temporizadores, fetch, etc.
 
-
 // Usando a API Fetch para fazer uma solicitação HTTP
 fetch('https://api.exemplo.com/dados')
     .then(Response => Response.json())
