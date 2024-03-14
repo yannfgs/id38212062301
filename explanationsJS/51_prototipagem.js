61a23b8a67fb24d780842dd27fbbd442084bf7d9
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-03-14 17:59:07 -0300

    14032402

--- explanationsJS/51_prototipagem.js
+++ explanationsJS/51_prototipagem.js
@@ -2,7 +2,6 @@
 
 // Em JavaScript, cada objeto tem um "protótipo". Se você tentar acessar uma propriedade que não existe no objeto, JavaScript tentará encontrá-la no protótipo do objeto.
 
-
 function Pessoa(nome) {
   this.nome = nome;
 }
