fef380d0b670489d7a7a8c6858a1e8f61a194ccb
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-03-16 12:07:53 -0300

    16032402

--- explanationsJS/60_transpiladores(como_Babel).js
+++ explanationsJS/60_transpiladores(como_Babel).js
@@ -2,7 +2,6 @@
 
 // Os transpiladores convertem o código de uma versão do JavaScript para outra. Por exemplo, o Babel pode converter o código ES6 para ES5 para compatibilidade com navegadores mais antigos. Isso permite que os desenvolvedores usem os recursos mais recentes do JavaScript, sem se preocupar com a compatibilidade do navegador. O Babel também pode converter o JSX para JavaScript comum, permitindo que os desenvolvedores usem o React sem se preocupar com a compatibilidade do navegador. O Babel pode ser usado como uma CLI, ou como uma dependência do Node.js. O Babel também pode ser usado como um plugin para Webpack, Rollup ou Gulp.
 
-
 // Código ES6
 const minhaFuncao = () => {
     console.log("Olá, mundo!");
