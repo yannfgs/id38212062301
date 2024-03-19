893313382a21a3307852645bc0288647e30fda3a
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-03-19 17:30:45 -0300

    19032401

--- explanationsJS/57_importacaoExportacaoModulosES6.js
+++ explanationsJS/57_importacaoExportacaoModulosES6.js
@@ -3,6 +3,7 @@
 // Com ES6, você pode importar e exportar funções, objetos ou valores de um módulo para outro.
 
 // arquivo: modulo.js
+
 export const nome = "João";
 export  function dizerOi() {
     console.log("Oi");
