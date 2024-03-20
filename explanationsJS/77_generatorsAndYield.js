c173478402f1479cf47c2448a3691eb8ecfba0f3
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-03-20 17:44:52 -0300

    20032403

--- explanationsJS/77_generatorsAndYield.js
+++ explanationsJS/77_generatorsAndYield.js
@@ -2,6 +2,7 @@
 
 // As funções geradoras são um tipo especial de função que pausa sua execução, permitindo que você as retorne posteriormente. O `yield` é usado para pausar e retornar uma função geradora.
 
+
 function* gerador() {
     yield 1;
     yield 2;
