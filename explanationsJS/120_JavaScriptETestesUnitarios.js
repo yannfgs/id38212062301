00db9696ea0100100edd034929e26f0a9435f57f
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-04-02 18:10:42 -0300

    02042401

--- explanationsJS/120_JavaScriptETestesUnitarios.js
+++ explanationsJS/120_JavaScriptETestesUnitarios.js
@@ -1,7 +1,6 @@
 // Exemplo usando Jest para testar uma função de soma
 // Este código deve estar em um arquivo de teste Jest, como sum.test.js
 
-
 const sum = (a, b) => a + b;
 
 test('soma de 1 + 2 deve resultar em 3', () => {
