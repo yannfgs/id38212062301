d8f4ce6dd4f4a4fb356bc85d95355c737b974a0a
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-04-03 18:34:35 -0300

    03042402

--- explanationsJS/118_JavaScripteComputaçãoGráfica.js
+++ explanationsJS/118_JavaScripteComputaçãoGráfica.js
@@ -2,7 +2,6 @@
 
 // Com WebGL e Three.js, JavaScript pode ser usado para criar gráficos 3D complexos no navegador.
 
-
 // Exemplo básico usando Three.js
 const cena = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
