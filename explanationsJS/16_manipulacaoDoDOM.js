6b05767088553b152360787b5c056b84df6db793
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-04-22 18:08:08 -0300

    22042403

--- explanationsJS/16_manipulacaoDoDOM.js
+++ explanationsJS/16_manipulacaoDoDOM.js
@@ -3,7 +3,6 @@
 // Suponha que temos o seguinte HTML:
 // <div id="meuDiv">Olá, mundo!</div>
 
-
 let div = document.getElementById("meuDiv"); // obtém o elemento com o id "meuDiv"
 console.log(div.textContent); // Olá, mundo!
 
