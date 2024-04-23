1e3f472f93357706850541143a008d263f4cef78
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-04-23 15:21:44 -0300

    23042402

--- explanationsJS/22_modulos.js
+++ explanationsJS/22_modulos.js
@@ -1,7 +1,6 @@
 // Módulos
 // Os módulos JavaScript são blocos de código reutilizáveis que exportam recursos específicos para serem usados em outros programas.
 
-
 // arquivo: modulo.js
 export function saudacao(nome) {
     return `Olá, ${nome}!`;
