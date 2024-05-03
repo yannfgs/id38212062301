ff219cba24ba7383056c7c48f61b8204dd95bb12
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-05-03 07:42:36 -0300

    03052401

--- explanationsJS/41_manipulacaoDeErrosComThrow.js
+++ explanationsJS/41_manipulacaoDeErrosComThrow.js
@@ -2,7 +2,6 @@
 
 // Você pode lançar suas próprias exceções em JavaScript usando a palavra-chave `throw`.
 
-
 function dividir(a, b) {
     if (b === 0) {
         throw new Error("Divisão por zero");
