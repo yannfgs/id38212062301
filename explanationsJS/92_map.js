58dfc0713a141918f04152a61bbb80dc2f7f3463
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-05-14 17:37:32 -0300

    14052401

--- explanationsJS/92_map.js
+++ explanationsJS/92_map.js
@@ -2,7 +2,6 @@
 
 // O objeto `Map` é uma coleção de pares chave/valor onde as chaves podem ser qualquer valor.
 
-
 const mapa = new Map();
 mapa.set('chave', 'valor');
 console.log(mapa.get('chave')); // "valor"
