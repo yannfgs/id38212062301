f2b881fc235019fbfae195e0e15c9b9b1ef7e2f5
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-05-17 17:30:08 -0300

    17052401

--- JSprojects/Project4_CalculadoraSimples/script.js
+++ JSprojects/Project4_CalculadoraSimples/script.js
@@ -2,6 +2,7 @@ function calcular() { // Define a função calcular
     const num1 = parseFloat(document.getElementById('num1').value); // Obtém o valor do primeiro input e converte para float
     const num2 = parseFloat(document.getElementById('num2').value); // Obtém o valor do segundo input e converte para float
 
+    
     if (!isNaN(num1) && !isNaN(num2)) { // Verifica se ambos os valores são números
         const soma = num1 + num2; // Soma os dois números
         document.getElementById('resultado').innerText = 'Resultado: ' + soma; // Exibe o resultado
