28c1d1169160b95ff7e1e1a2915438e3ed8ef22c
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-05-18 12:11:51 -0300

    18052404

--- explanationsJS/96_templateStrings.js
+++ explanationsJS/96_templateStrings.js
@@ -2,7 +2,6 @@
 
 // Template strings permitem a criação de strings complexas com mais facilidade.
 
-
 const nome = 'João';
 const saudacao = `Olá, ${nome}!`;
 console.log(saudacao); // "Olá, João!"
