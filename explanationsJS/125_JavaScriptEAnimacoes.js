936bf7c4ef889ff3c70dd532834f5590d251081b
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-05-20 18:09:37 -0300

    2005202403

--- explanationsJS/125_JavaScriptEAnimacoes.js
+++ explanationsJS/125_JavaScriptEAnimacoes.js
@@ -2,7 +2,6 @@
 
 // JavaScript pode ser usado para criar animações interativas na web, utilizando `requestAnimationFrame` para eficiência.
 
-
 // Animação básica que move um elemento da esquerda para a direita
 let pos = 0;
 const meuElemento = document.getElementById('meuElemento');
