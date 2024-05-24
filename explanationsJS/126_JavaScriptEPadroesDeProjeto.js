3b89281d1dadf457bc48d876c1a17f7dcdd7136c
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-05-24 19:30:18 -0300

    24052402

--- explanationsJS/126_JavaScriptEPadroesDeProjeto.js
+++ explanationsJS/126_JavaScriptEPadroesDeProjeto.js
@@ -1,7 +1,8 @@
 // JavaScript e Padrões de Projeto
 
 // Padrões de projeto são soluções reutilizáveis para problemas comuns em desenvolvimento de software. Um exemplo é o padrão Singleton.
-''
+
+
 // Implementação básica do padrão Singleton em JavaScript
 class Singleton {
     constructor() {
