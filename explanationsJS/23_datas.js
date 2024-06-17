cea2cbc5237738483bdd4a5c5eeaced694558967
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-06-17 17:41:53 -0300

    17062403

--- explanationsJS/23_datas.js
+++ explanationsJS/23_datas.js
@@ -1,6 +1,7 @@
 // Datas
 // O JavaScript tem um objeto Date embutido que você pode usar para criar, recuperar e manipular datas.
 
+
 let agora = new Date();
 console.log(agora); // exibe a data e hora atuais
 
