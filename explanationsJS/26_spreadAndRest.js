0ffbe66bc1458b48c2fa77046cb7ec41b1007c84
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-06-18 17:38:34 -0300

    18062404

--- explanationsJS/26_spreadAndRest.js
+++ explanationsJS/26_spreadAndRest.js
@@ -2,7 +2,6 @@
 
 // O operador spread (`...`) é usado para "espalhar" os elementos de um array (ou as propriedades de um objeto) em outro lugar. O operador rest (`...`) é usado para coletar o "resto" dos elementos de um array (ou as propriedades de um objeto) em outra variável.
 
-
 let arr1 = [1, 2, 3];
 let arr2 = [...arr1, 4, 5, 6]; // [1, 2, 3, 4, 5, 6]
 console.log(arr2);
