de9d934ed083b1f33660d58e0610e0ed59a50d0e
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-07-13 11:50:43 -0300

    13042402

--- explanationsJS/2_tiposDeDados.js
+++ explanationsJS/2_tiposDeDados.js
@@ -1,6 +1,5 @@
 // JavaScript tem vários tipos de dados, mas os mais comuns são números, strings e booleanos.
 
-
 let numero = 10; // numero
 let texto = "Olá, mundo!" // string
 let estaChovendo = false; // booleano
