9442d21d86d9c044de2df4074a57ac59a7784148
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-07-17 17:24:32 -0300

    17072401

--- explanationsJS/21_classes.js
+++ explanationsJS/21_classes.js
@@ -1,7 +1,6 @@
 // Classes
 // As classes são um recurso do ES6 que fornece uma sintaxe mais clara e simples para criar objetos e lidar com a herança.
 
-
 class Pessoa {
     constructor(nome, idade) {
         this.nome = nome;
