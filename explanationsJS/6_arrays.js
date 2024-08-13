593e89beca86e7365ea79ac0dfb0d5557287bb52
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-08-13 18:20:48 -0300

    13082401

--- explanationsJS/6_arrays.js
+++ explanationsJS/6_arrays.js
@@ -2,9 +2,9 @@
 
 // A contagem de arrays se inicia no 0, portanto a posição 1 do array é o segundo elemento.
 
+
 let numeros = [1, 2, 3, 4, 5];
 console.log(numeros[0]); // 1
 
 let misto = [1, "dois", false, { nome: "João" }, [5, 6, 7]];
 console.log(misto[3].nome); // João
-
