5c6908e4e8c91e3a2496a2ad82eb9f87326b5818
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-08-13 18:20:56 -0300

    13082402

--- explanationsJS/13_funcoesDeAltaOrdem.js
+++ explanationsJS/13_funcoesDeAltaOrdem.js
@@ -1,6 +1,5 @@
 // Em JavaScript, as funções são cidadãos de primeira classe, o que significa que podem ser passadas como argumentos para outras funções e podem ser retornadas por outras funções. Uma função que aceita outras funções como argumentos ou retorna uma função é conhecida como uma função de alta ordem.
 
-
 function saudacao(nome) {
     return `Olá, ${nome}!`;
 }
