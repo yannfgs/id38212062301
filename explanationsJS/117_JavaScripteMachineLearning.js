e0ec775a0fbca15c5057174d5e39cc55538d38c2
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-08-22 18:18:55 -0300

    22082401

--- explanationsJS/117_JavaScripteMachineLearning.js
+++ explanationsJS/117_JavaScripteMachineLearning.js
@@ -1,6 +1,5 @@
 // JavaScript e Machine Learning
 
-
 // Com bibliotecas como TensorFlow.js, é possível executar modelos de machine learning no navegador ou em Node.js.
 
 // Exemplo usando TensorFlow.js
