7eff79435c6aba8c025261f5dc69f35bfacc5865
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-08-23 18:30:33 -0300

    23082402

--- explanationsJS/121_JavaScriptESVG.js
+++ explanationsJS/121_JavaScriptESVG.js
@@ -2,6 +2,7 @@
 
 // SVG permite criar gráficos vetoriais escaláveis, que você pode controlar com JavaScript.
 
+
 document.addEventListener("DOMContentLoaded", function (event) {
     setTimeout(function () {
         document.getElementById('meu-circulo').style.fill = 'blue';
